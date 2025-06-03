406b35b1dff804e21f3079917a83fe6fa08f0531
hala <hala.sebbah@nestle.com>
2025-06-03 03:46:14 +0100

    TEST-3 commit in branch created from ticket 4

--- SimpleJavaProject/src/main/java/org/example/Main.java
+++ SimpleJavaProject/src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        // test with link to work item in jira 4
+        // test with link to work item in jira 5
         System.out.print("Hello and welcome in branch TEST-3");
 
     }
